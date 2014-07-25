be197098fffde12fd67a0727d6c7f4bd2361946e
seri <seri.wb@gmail.com>
2014-07-25 23:00:53 +0900

    made TwitterWatcher
    画像URL取得クラスを作成（とりあえず動作する）

--- src/main/groovy/white/box/reins/TwitterWatcher.groovy
+++ src/main/groovy/white/box/reins/TwitterWatcher.groovy
@@ -18,8 +18,14 @@ class TwitterWatcher extends Thread {
 
 	def config = null
 
+	private boolean loop = true
+
 	TwitterWatcher(def config) {
 		this.config = config
+	}
+
+	public void stopRunning(){
+		loop = false;
 	}
 
 	@Override
@@ -37,18 +43,23 @@ class TwitterWatcher extends Thread {
 		final int tweet_max_count = config.reins.tweet.maxcount
 
 		// リストごとにスリープ、リスト終わって長めのスリープ
-		boolean loop = true
-		int counter = 0
-		while(loop && counter++ < 4) {
+		int waittime = 1000		// TODO:外部読み込みに
+
+		int counter = 0		// TODO:テスト用
+		while(loop && counter++ < 3) {
 
 			// 認証ユーザが持つリストを取得
 			ResponseList<UserList> lists = twitter.getUserLists(userinfo.getScreenName())
+
+			// TODO:list_mstが持つリスト名をどこかのタイミングで更新するようにすること
+			// TODO:リストのブラック、ホワイトリストを持つようにした場合、そこのチェックタイミングで更新すること
+
 
 			// リストごとに情報を取得
 			Paging paging = new Paging(1, tweet_max_count)
 			lists.each { list ->
 
-				// list_idでマスタを探し、存在していればテーブルを作成しない。存在しなければ作成する。
+				// list_idでマスタを探し、存在しなければリスト用のテーブルを作成する。
 				long listId = list.getId()
 				String listname = list.getName()
 
@@ -57,91 +68,77 @@ class TwitterWatcher extends Thread {
 					listDataDao.create(listId)
 				}
 
+				// 現在チェックしているところまでのsince_idを設定
+				// マスタのsinceIdがnullでなければpagingする
+				long currentSinceId = listMstDao.getSinceId(listId) ?: -1
+
 				// --------------- ツイート取得して解析 -----------------
-				ResponseList<Status> statuses = twitter.getUserListStatuses(listId, paging)
-				statuses.each { status ->
+				int paging_max_count = 1
+				if (currentSinceId != -1) {
+					paging.sinceId = currentSinceId
+					paging_max_count = 10
+				}
 
-					def screenName = status.getUser().getScreenName()
-					// 共通的な値はあらかじめ入れる
-					ListData listdata = new ListData(
-									screenName:screenName,
-									counterStatus:0,
-									tweetUrl:"https://twitter.com/${screenName}/status/${status.getId()}",
-									tweetDate:status.getCreatedAt())
+				println "current since_id:" + currentSinceId
 
-					//					String text = status.getText()
+				// 最大200×10ツイートの取得
+				for (int i=1; i <= paging_max_count; i++) {
+					paging.page = i
+					ResponseList<Status> statuses = twitter.getUserListStatuses(listId, paging)
 
-					// media_urlならTwitter公式、それ以外は別形式で保存
-					def mediaList = status.getMediaEntities()
-					mediaList.each {
-						listdata.url = it.getMediaURL()
-						listdata.attribute = "twitter"
-						listDataDao.insert(listId, listdata)
+					if (statuses == null || statuses.size() == 0) {
+						break
 					}
-					def urlsList = status.getURLEntities()
-					urlsList.each {
-						// pixivリンクの保存
-						if(it.getExpandedURL() =~ """www.pixiv.net/member_illust.php""") {
-							listdata.url = it.getExpandedURL()
-							listdata.attribute = "pixiv"
+
+					for (Status status : statuses) {
+//					statuses.each { status ->
+
+						def screenName = status.getUser().getScreenName()
+						// 共通的な値はあらかじめ入れる
+						ListData listdata = new ListData(
+										screenName : screenName,
+										counterStatus : 0,
+										statusId : status.getId(),
+										tweetDate : status.getCreatedAt())
+
+						// media_urlならTwitter公式、それ以外は別形式で保存
+						def mediaList = status.getMediaEntities()
+						mediaList.each {
+							listdata.url = it.getMediaURL()
+							listdata.attribute = "twitter"
 							listDataDao.insert(listId, listdata)
 						}
-						// gifの保存
-						else if (it.getExpandedURL() =~ /.gif$/) {
-							listdata.url = it.getExpandedURL()
-							listdata.attribute = "gif"
-							listDataDao.insert(listId, listdata)
+						// ----------■公式以外のリンクを取得する場合はここに書く--------------
+						def urlsList = status.getURLEntities()
+						urlsList.each {
+							// pixivリンクの保存
+							if(it.getExpandedURL() =~ """www.pixiv.net/member_illust.php""") {
+								listdata.url = it.getExpandedURL()
+								listdata.attribute = "pixiv"
+								listDataDao.insert(listId, listdata)
+							}
+							// gifの保存
+							else if (it.getExpandedURL() =~ /.gif$/) {
+								listdata.url = it.getExpandedURL()
+								listdata.attribute = "gif"
+								listDataDao.insert(listId, listdata)
+							}
 						}
+						// ---------------------------------------------------------------------
+					}
+
+					if (i==1) {
+						// since_idの保持
+						listMstDao.updateSinceId(listId, statuses.get(0).getId())
 					}
 				}
 
 				// リストごとにちょっと待つ
-				Thread.sleep(1000)
+				Thread.sleep(waittime * 10)
 			}
 
 			// 1周したら結構待つ
-			Thread.sleep(3000)
-
-			// TODO:エンド処理
-//			loop = false
+			Thread.sleep(waittime * 600)
 		}
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//		List<Status> statuses = twitter.getHomeTimeline()
-	//
-	//		statuses.each {
-	//			status ->
-	//			println(status.getUser().getName() + ":" + status.getText());
-	//		}
-
-
-	// TODO:リンクをリストに取得（URL、ユーザ名、日時
-
-	// TODO:body直下がimgタグだったらsrcを取ればいい
-	// TODO:pixivのまとまったやつはimgタグのdata-srcを取ればよい
-
-	//		.each {
-	//			it.each {
-	//				println "id:" + it.getId() + ", name:" + it.getName() + ", description:" + it.getDescription() + ", slug:" + it.getSlug() + " /end"
-	//			}
-	//		}
-	def getList(UserList list) {
-	}
-
-
 }
